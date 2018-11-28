a0adf2f44f4def3cbdd3398a12d0f5ca405fcc84
Ariel <aramirez@armit-solutions.com>
2018-11-28 16:35:39 -0600

    last changes: move snippet of tiles

--- readDir.js
+++ readDir.js
@@ -3,54 +3,6 @@ const fs = require('fs'),
       sqlite = require('./sqlite.js'),
       rl = require('./readLine.js');
 
-/*exports.readDir = function(pathDir) {
-  path.mainPath(pathDir);//root_dir
-  readPathDir(pathDir, (lotes) => {
-    lotes.forEach(lote => {
-      const pathToLote = pathDir + '/' + lote;//lote
-      if((fs.lstatSync(pathToLote).isDirectory()) && (lote.charAt(0) != '.')) {
-        readPathDir(pathToLote,(cuadrants) => {
-          cuadrants.forEach(cuadrant => {
-            const pathToCuadrant = pathToLote + '/' + cuadrant;//cuadrant
-            if((fs.lstatSync(pathToCuadrant).isDirectory()) && (cuadrant.charAt(0) != '.')) {
-              readPathDir(pathToCuadrant,(levels_zoom) => {
-                levels_zoom.forEach(level_zoom => {
-                  const pathToLevel_zoom = pathToCuadrant + '/' + level_zoom; //level_zoom
-                  if((fs.lstatSync(pathToLevel_zoom).isDirectory()) && (level_zoom.charAt(0) != '.')) {
-                    readPathDir(pathToLevel_zoom,(dirsBF) => {
-                      dirsBF.forEach(dirBF => {
-                        const pathToDirBF = pathToLevel_zoom + '/' + dirBF; //directory before Files.png
-                        if(fs.lstatSync(pathToDirBF).isDirectory()) {
-                          readPathDir(pathToDirBF, (pngs) => {
-                            pngs.forEach(png => {
-                              const pathToPNG = pathToDirBF + '/' + png;
-                              const fullPathObj = {
-                                root_dir: pathDir,
-                                lote: path.basename(pathToLote),
-                                cuadrant: path.basename(pathToCuadrant),
-                                level_zoom: path.basename(pathToLevel_zoom),
-                                dir1: path.basename(pathToDirBF),
-                                fileName: path.basename(pathToPNG)
-                              }
-                              //call SQLite method
-                              sqlite.insertRecord(fullPathObj);
-                            });
-                          });
-                        }
-                      });
-                    });
-                  }
-                });
-              });
-            }
-          });
-        });
-      }
-    });
-    rl.prompt();
-  });
-}*/
-
 function readPathDir(pathToRead,callback){
   fs.readdir(pathToRead, (err, files) => {
     if (files != null){
